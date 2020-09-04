65e13778014f6dddb8546badc453540d09aae229
CucoRegresa <nicoizquierdo95@gmail.com>
2020-09-04 17:14:18 -0400

    Clean Huffman

--- headers/huffman.hpp
+++ headers/huffman.hpp
@@ -1,39 +1,20 @@
-
-
 #include <map>
 #include <string>
 #include <vector>
 using namespace std;
 namespace huffman {
 
-// This constant can be avoided by explicitly
-// calculating height of Huffman Tree
 #define MAX_TREE_HT 100
 
-// A Huffman tree node
     typedef struct MinHeapNode {
-
-        // One of the input characters
         int number;
-
-        // Frequency of the character
         int freq;
-
-        // Left and right child of this node
         struct MinHeapNode *left, *right;
     } MinHeapNode;
 
-// A Min Heap: Collection of
-// min-heap (or Huffman tree) nodes
     typedef struct MinHeap {
-
-        // Current size of min heap
         unsigned size;
-
-        // capacity of min heap
         unsigned capacity;
-
-        // Attay of minheap node pointers
         struct MinHeapNode** array;
     } MinHeap;
 
